a3acbb9b2b9de55faff84d7ebfb727b850be8ebe
Jeremias <jeremiasmangi247@gmail.com>
2019-09-15 14:48:12 -0300

    1.0
    Primeros pasos

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('h1').addEventListener('click', ()=> {
+    document.body.style.backgroundColor = 'black' ;
+})
+document.querySelector('p').addEventListener('click', ()=> {
+    document.body.style.background = 'black' ;
+})
